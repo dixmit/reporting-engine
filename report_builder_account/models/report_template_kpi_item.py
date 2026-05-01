d5a611dce1056a898cb0ee7d64d450e3d502373b
Enric Tobella <enric.tobella@dixmit.com>
2026-05-01 19:30:37 +0200

    [ADD] report_builder_account

---
+++ report_builder_account/models/report_template_kpi_item.py
@@ -0,0 +1,118 @@
+# Copyright 2025 Dixmit
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+from odoo import models
+from odoo.osv.expression import OR
+from odoo.tools.safe_eval import safe_eval
+
+
+class ReportTemplateKpiItem(models.Model):
+    _inherit = "report.template.kpi.item"
+
+    def _get_kpi_account_domain(self, col, date_format="balance"):
+        domain = []
+        if self.domain:
+            domain += safe_eval(self.domain)
+        if self.code:
+            domain += OR(
+                [
+                    [("account_id.code", "=ilike", code.strip())]
+                    for code in self.code.split(",")
+                ]
+            )
+        if date_format == "balance":
+            domain += [
+                ("date", ">=", col["date_from"]),
+                ("date", "<=", col["date_to"]),
+            ]
+        elif date_format == "initial":
+            domain += [
+                ("date", "<", col["date_from"]),
+            ]
+        elif date_format == "ending":
+            domain += [
+                ("date", "<=", col["date_to"]),
+            ]
+        return domain
+
+    def _get_kpi_value_account_balp(self, col):
+        domain = self._get_kpi_account_domain(col, date_format="balance")
+        values = self.env["account.move.line"]._read_group(
+            domain, groupby=("account_id", "company_id"), aggregates=("balance:sum",)
+        )
+        return sum(val[2] for val in values), {
+            f"{val[0].code} - {val[0].name} [{val[1].name}]": val[2] for val in values
+        }
+
+    def _get_kpi_value_account_bali(self, col):
+        domain = self._get_kpi_account_domain(col, date_format="initial")
+        values = self.env["account.move.line"]._read_group(
+            domain, groupby=("account_id", "company_id"), aggregates=("balance:sum",)
+        )
+        return sum(val[2] for val in values), {
+            f"{val[0].code} - {val[0].name} [{val[1].name}]": val[2] for val in values
+        }
+
+    def _get_kpi_value_account_bale(self, col):
+        domain = self._get_kpi_account_domain(col, date_format="ending")
+        values = self.env["account.move.line"]._read_group(
+            domain, groupby=("account_id", "company_id"), aggregates=("balance:sum",)
+        )
+        return sum(val[2] for val in values), {
+            f"{val[0].code} - {val[0].name} [{val[1].name}]": val[2] for val in values
+        }
+
+    def _get_kpi_value_account_crdp(self, col):
+        domain = self._get_kpi_account_domain(col, date_format="balance")
+        values = self.env["account.move.line"]._read_group(
+            domain, groupby=("account_id", "company_id"), aggregates=("credit:sum",)
+        )
+        return sum(val[2] for val in values), {
+            f"{val[0].code} - {val[0].name} [{val[1].name}]": val[2] for val in values
+        }
+
+    def _get_kpi_value_account_crdi(self, col):
+        domain = self._get_kpi_account_domain(col, date_format="initial")
+        values = self.env["account.move.line"]._read_group(
+            domain, groupby=("account_id", "company_id"), aggregates=("credit:sum",)
+        )
+        return sum(val[2] for val in values), {
+            f"{val[0].code} - {val[0].name} [{val[1].name}]": val[2] for val in values
+        }
+
+    def _get_kpi_value_account_crde(self, col):
+        domain = self._get_kpi_account_domain(col, date_format="ending")
+        values = self.env["account.move.line"]._read_group(
+            domain, groupby=("account_id", "company_id"), aggregates=("credit:sum",)
+        )
+        return sum(val[2] for val in values), {
+            f"{val[0].code} - {val[0].name} [{val[1].name}]": val[2] for val in values
+        }
+
+    def _get_kpi_value_account_debp(self, col):
+        domain = self._get_kpi_account_domain(col, date_format="balance")
+        values = self.env["account.move.line"]._read_group(
+            domain, groupby=("account_id", "company_id"), aggregates=("debit:sum",)
+        )
+        return sum(val[2] for val in values), {
+            f"{val[0].code} - {val[0].name} [{val[1].name}]": val[2] for val in values
+        }
+
+    def _get_kpi_value_account_debi(self, col):
+        domain = self._get_kpi_account_domain(col, date_format="initial")
+        values = self.env["account.move.line"]._read_group(
+            domain, groupby=("account_id", "company_id"), aggregates=("debit:sum",)
+        )
+        return sum(val[2] for val in values), {
+            f"{val[0].code} - {val[0].name} [{val[1].name}]": val[2] for val in values
+        }
+
+    def _get_kpi_value_account_debe(self, col):
+        domain = self._get_kpi_account_domain(col, date_format="ending")
+        values = self.env["account.move.line"]._read_group(
+            domain, groupby=("account_id", "company_id"), aggregates=("debit:sum",)
+        )
+        return sum(val[2] for val in values), {
+            f"{val[0].code} - {val[0].name} [{val[1].name}]": val[2] for val in values
+        }
