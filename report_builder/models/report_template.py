4c79a1e8206fa43f75d6028ebd61f5a00344675d
Enric Tobella <enric.tobella@dixmit.com>
2026-05-02 15:24:50 +0200

    [FIX] report_builder: Fix search model & view filter

--- report_builder/models/report_template.py
+++ report_builder/models/report_template.py
@@ -22,7 +22,8 @@ class ReportTemplate(models.Model):
     )
     search_view_id = fields.Many2one(
         "ir.ui.view",
-        domain="[('type', '=', 'search'), ('model_id', '=', search_model_id)]",
+        domain="[('type', '=', 'search'), ('model', '=', search_model)]",
     )
     search_model_id = fields.Many2one("ir.model")
+    search_model = fields.Char(related="search_model_id.model")
     style_id = fields.Many2one("report.style")
